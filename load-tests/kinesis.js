e13fec63df59deab1c710b0171b3609f84b1f084
mkovenko <mkovenko@n-ix.com>
2023-08-28 06:21:57 +0200

    Reduced input for now

--- load-tests/kinesis.js
+++ load-tests/kinesis.js
@@ -3,7 +3,8 @@ import { generateTelemetry } from './data.js';
 import { generateScenario } from './scenario.js';
 
 // Each car sends 1 request per second
-export const options = generateScenario({ numberOfCars: 10000 });
+// export const options = generateScenario({ numberOfCars: 10000 });
+export const options = generateScenario({ numberOfCars: 300 });
 
 const url = 'https://lcxca1s245.execute-api.eu-central-1.amazonaws.com/demo/telemetry';
 
