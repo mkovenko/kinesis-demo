2b9c1cea09071e56bf26c85f1371ab2abd91d7c5
mkovenko <mkovenko@n-ix.com>
2023-08-29 15:39:17 +0200

    Updated the url

--- load-tests/kinesis.js
+++ load-tests/kinesis.js
@@ -6,7 +6,7 @@ import { generateScenario } from './scenario.js';
 // export const options = generateScenario({ numberOfCars: 10000 });
 export const options = generateScenario({ numberOfCars: 1000 });
 
-const url = 'https://198sfge5z8.execute-api.eu-central-1.amazonaws.com/demo/telemetry';
+const url = 'https://1md8tjr67l.execute-api.eu-central-1.amazonaws.com/demo/telemetry';
 
 export default function () {
   const payload = generateTelemetry();
