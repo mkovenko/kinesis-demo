b5c54c37e4852b167bc87a11935dbc2012776a12
mkovenko <mkovenko@n-ix.com>
2023-09-01 07:31:49 +0200

    Added debug

--- src/old-architecture/queue-service.js
+++ src/old-architecture/queue-service.js
@@ -3,8 +3,13 @@ import {
   SendMessageCommand,
 } from '@aws-sdk/client-sqs';
 
-const QueueUrl =  process.env.PDF_GENERATION_QUEUE_URL;
+const QueueUrl =  process.env.QUEUE_URL;
 const sqsClient = new SQSClient({ region: process.env.AWS_DEFAULT_REGION });
+
+console.log('===============================');
+console.log(process.env.QUEUE_URL);
+console.log(process.env.AWS_DEFAULT_REGION);
+console.log('===============================');
 
 export const enqueueTelemetry = async (telemetry) => {
   const command = new SendMessageCommand({
